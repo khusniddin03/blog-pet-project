d748d9d7ed47f758ffd5848ed0c7aa6eb3def213
Abdupattoyev Khusniddin <husniddinuzinfocom@gmail.com>
2024-03-04 00:03:43 +0500

    fix linter

--- src/entities/counter/index.ts
+++ src/entities/counter/index.ts
@@ -1,9 +1,9 @@
-import Counter from "./ui/counter";
+import Counter from './ui/counter';
 import { ICounterSchema } from './model/types';
 import { counterReducer } from './model/slice/counterSlice';
 
 export {
     Counter,
     ICounterSchema,
-    counterReducer
-}
+    counterReducer,
+};
